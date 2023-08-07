e08fb5bb33202cea25e0308a92050cb24e65e4d0
Luan Cordeiro <luam.lc19@gmail.com>
2023-08-07 18:51:26 -0300

    style: set default font family

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import { Lato } from 'next/font/google'
+
+const lato = Lato({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  display: 'swap'
+})
 
 export const metadata: Metadata = {
   title: 'lcstore',
@@ -12,8 +19,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="pt-BR">
-      <body>{children}</body>
+    <html lang="pt-BR" className={lato.className}>
+      <body>
+        {children}
+      </body>
     </html>
   )
 }
