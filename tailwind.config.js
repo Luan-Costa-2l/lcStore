d07018d1d0cc17c376d4d3f43502aa71b3b5894e
Luan Cordeiro <luam.lc19@gmail.com>
2023-08-10 16:41:25 -0300

    chore(tailwind): extend theme and add default with

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,17 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    backgroundColor: {
-      'blue-light': '#0086FF',
-      'blue-dark': '#0468C3',
-      'green': '#59C00B',
-      'green-dark': '#3A7808'
-    }
+    extend: {
+      backgroundColor: {
+        'blue-light': '#0086FF',
+        'blue-dark': '#0468C3',
+        'green': '#59C00B',
+        'green-dark': '#3A7808'
+      },
+      maxWidth: {
+        'default': '1200px'
+      },
+    },
   },
   plugins: [],
 }
