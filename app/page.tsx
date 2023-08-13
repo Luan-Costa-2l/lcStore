2aa6c02d73460a59acc641f28eeb9ff35af8300c
Luan Cordeiro <luam.lc19@gmail.com>
2023-08-13 09:33:28 -0300

    feat: add ads on screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
 import { SearchArea } from "@/components/SearchArea";
+import api from '@/api';
+import { Ad } from "@/components/Ad";
 
-export default function Home() {
+const Home = async () => {
+  const adData = await api.getAds();
+
   return (
-    <main>
+    <main className="bg-gray-100 min-h-[calc(100vh-60px)]">
       <SearchArea />
-      <div className="max-w-default mx-auto py-6">
-        <h1 className="font-bold text-2xl">Anúncios recentes</h1>
+      <div className="px-5 py-6">
+        <section className="max-w-default mx-auto">
+          <h1 className="font-bold text-2xl pb-5">Anúncios recentes</h1>
+          <div className="grid grid-cols-4 gap-10">
+            {adData && adData.ads && adData.ads.map(item => (
+              <Ad id={item.id} url={item.image} title={item.title} price={item.price} key={item.id} />
+            ))}
+          </div>
+        </section>
       </div>
     </main>
   )
 }
+
+
+export default Home;
