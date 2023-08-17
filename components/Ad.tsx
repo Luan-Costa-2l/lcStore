fab1f317fb9b7fe6da22b41f5f270739a4769b44
Luan Cordeiro <luam.lc19@gmail.com>
2023-08-17 18:42:08 -0300

    style: add a border on ad component

--- components/Ad.tsx
+++ components/Ad.tsx
@@ -10,7 +10,7 @@ type Props = {
 export const Ad = async ({ id, url, title, price }: Props) => {
     return (
         <Link href={`/ads/${id}`} className="bg-white p-3 rounded-lg hover:shadow-lg transition-shadow border-[1px] border-gray-300">
-            <img src={url} alt={title} className="rounded-t-lg" />
+            <img src={url} alt={title} className="rounded-t-lg border-[1px] border-gray-300" />
             <div>
                 <p className="py-1">{title}</p>
                 <div className="font-bold text-xl">{price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'})}</div>
