c1f717ff28110e708279df0a0eaf6b0144f25108
Luan Cordeiro <luam.lc19@gmail.com>
2023-08-22 11:15:05 -0300

    feat: create a signup function

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import { AdType, Category, State } from "@/types";
 const BASE_URL = process.env.NODE_ENV == 'production' ? 'https://lcstore-api.onrender.com' : 'http://localhost:8080'; // open url
 
 const formatQueryFilters = (sort?: 'asc' | 'desc', offset?: number, limit?: string, q?: string, cat?: string, state?: string) => {
-    let filters: string[]  = [];
+    let filters: string[] = [];
     if (sort) {
         filters.push(`sort=${sort}`);
     }
@@ -37,6 +37,18 @@ interface GetAdsParams {
     state?: string
 }
 
+
+interface ErrorResponseType {
+    error: { [tag: string]: { msg: string } }
+}
+
+export interface SignupParamsType {
+    name: string;
+    email: string;
+    state: string;
+    password: string;
+}
+
 export default {
     getCategories: async () => {
         const response: {categories: Category[]} = await fetch(BASE_URL + '/categories').then(res => res.json());
@@ -51,5 +63,24 @@ export default {
         const response: { ads: AdType[], total: number } = await fetch(BASE_URL + '/ad/list' + query, { next: { revalidate: 60 * 60 * 2 } })
             .then(res => res.json());
         return response;
+    },
+    signup: async ({ name, email, state, password }: SignupParamsType) => {
+        try {
+            const response = await fetch(BASE_URL + '/user/signup', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ name, email, state, password })
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData || 'Erro desconhecido');
+            }
+            const responseData: ErrorResponseType | { token: string } = await response.json();
+            return responseData;
+        } catch (error) {
+            console.error('Erro durante o registro: ', error);
+            throw new Error('Ocorreu um erro durante o registro, tente novamente mais tarde.');
+        }
     }
 }
