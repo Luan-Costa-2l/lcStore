a5b01fda38df1769b4d654d90d340a27387daaee
Luan Cordeiro <luam.lc19@gmail.com>
2023-08-22 11:27:15 -0300

    refactor: fieldSelect has a disabled default value

--- components/Field/FieldSelect.tsx
+++ components/Field/FieldSelect.tsx
@@ -14,6 +14,7 @@ export const FieldSelect = ({ name, data, value, setValue }: SelectProps) => {
         onChange={e => setValue(e.target.value)} 
         className="border-[1px] border-gray-300 py-1 px-2 rounded outline-1 outline-gray-400"
       >
+        <option value="" disabled>Selecionar</option>
         {data.length && data.map(item => (
           <option value={item._id} key={item._id}>{item.name}</option>
         ))}
