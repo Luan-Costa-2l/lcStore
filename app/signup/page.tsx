b18a9b35c823d93cb856b064661b2c2d16bf1ac6
Luan Cordeiro <luam.lc19@gmail.com>
2023-09-04 19:25:38 -0300

    fix(SignupPage): set new image size and centralize

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -6,12 +6,12 @@ const Signup = async () => {
     <main className="bg-gray-100 min-h-[calc(100vh-120px)] p-5">
       <section className="max-w-default mx-auto">
         <h1 className="font-bold text-2xl mb-5">Realizar cadastro</h1>
-        <div className="bg-white p-5 flex text-sm rounded-lg border-[1px] border-gray-300">
+        <div className="bg-white p-5 flex items-center text-sm rounded-lg border-[1px] border-gray-300">
 
           <SignupForm />
 
           <div className="w-full flex justify-center">
-            <Image src="/login.jpg" alt="Login image" width={280} height={280} className="rounded-full" priority />
+            <Image src="/login.jpg" alt="Login image" width={280} height={280} className="rounded-full h-full w-auto max-h-80" priority />
           </div>
         </div>
       </section>
