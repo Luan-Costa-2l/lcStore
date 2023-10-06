b9494b50d1bfff8ae93a4f328a591885ed83b348
Luan Cordeiro <luam.lc19@gmail.com>
2023-10-06 19:58:49 -0300

    feat(Button): extends props and recive bg-color

--- components/Field/FieldButton.tsx
+++ components/Field/FieldButton.tsx
@@ -1,11 +1,15 @@
-interface FieldButttonProps {
-  type: 'submit' | 'reset' | 'button' | undefined;
+import { ComponentProps } from "react";
+
+interface FieldButttonProps extends ComponentProps<'button'> {
   title: string;
+  color?: 'green' | 'blue';
 }
 
-export const FieldButton = ({ type=undefined, title }: FieldButttonProps) => {
+export const FieldButton = ({ title, color='blue', ...props }: FieldButttonProps) => {
+  const bgColor = color === 'blue' ? 'bg-blue-light' : 'bg-green';
+  const bgColorHover = color === 'blue' ? 'bg-blue-dark' : 'bg-green-dark';
   return (
-    <button type={type} className="bg-blue-light text-white py-2 px-7 rounded-lg transition-colors hover:bg-blue-dark">
+    <button {...props} className={`${bgColor} text-white py-2 px-7 rounded-lg transition-colors hover:${bgColorHover}`}>
       {title}
     </button>
   )
